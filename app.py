038a5d15b58835921107f918878cb88d146aa78e
sambit_ghosh_98 <sambitarghya@gmail.com>
2022-12-17 10:48:39 +0530

    dev: made hello world

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Hello World'
+
+if __name__ == "__main__":
+ app.run(debug=False,port=5001) #debug=False,port=5001
