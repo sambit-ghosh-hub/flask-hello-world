dab1ebbb1173f9985b6d571c00b004f93d18317c
sambit_ghosh_98 <sambitarghya@gmail.com>
2022-12-18 14:10:10 +0530

    changed 10

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask,jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Hello World changed yet again 2 18th dec'
+  return jsonify({"text":'Hello World changed 10'})
 
 if __name__ == "__main__":
  app.run(debug=False,port=5001) #debug=False,port=5001
